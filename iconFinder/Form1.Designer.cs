322edceaa2571151b186c79276396f332d6a99b4
Tom <tom.de.groot04@gmail.com>
2021-02-15 11:00:51 +0100

    Add project files.

---
+++ iconFinder/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace iconFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tbHero = new System.Windows.Forms.TextBox();
+            this.tbAbility = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.rbAbility = new System.Windows.Forms.RadioButton();
+            this.rbPortrait = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(97, 64);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(174, 25);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // tbHero
+            // 
+            this.tbHero.Location = new System.Drawing.Point(97, 12);
+            this.tbHero.Name = "tbHero";
+            this.tbHero.Size = new System.Drawing.Size(174, 20);
+            this.tbHero.TabIndex = 1;
+            // 
+            // tbAbility
+            // 
+            this.tbAbility.Location = new System.Drawing.Point(97, 38);
+            this.tbAbility.Name = "tbAbility";
+            this.tbAbility.Size = new System.Drawing.Size(174, 20);
+            this.tbAbility.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hero Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ability Name";
+            // 
+            // pbImage
+            // 
+            this.pbImage.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.pbImage.Location = new System.Drawing.Point(277, 12);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(128, 128);
+            this.pbImage.TabIndex = 5;
+            this.pbImage.TabStop = false;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Enabled = false;
+            this.btnCopy.Location = new System.Drawing.Point(277, 146);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(128, 25);
+            this.btnCopy.TabIndex = 6;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(277, 177);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(128, 25);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // rbAbility
+            // 
+            this.rbAbility.AutoSize = true;
+            this.rbAbility.Checked = true;
+            this.rbAbility.Location = new System.Drawing.Point(97, 95);
+            this.rbAbility.Name = "rbAbility";
+            this.rbAbility.Size = new System.Drawing.Size(52, 17);
+            this.rbAbility.TabIndex = 8;
+            this.rbAbility.TabStop = true;
+            this.rbAbility.Text = "Ability";
+            this.rbAbility.UseVisualStyleBackColor = true;
+            // 
+            // rbPortrait
+            // 
+            this.rbPortrait.AutoSize = true;
+            this.rbPortrait.Location = new System.Drawing.Point(97, 118);
+            this.rbPortrait.Name = "rbPortrait";
+            this.rbPortrait.Size = new System.Drawing.Size(84, 17);
+            this.rbPortrait.TabIndex = 9;
+            this.rbPortrait.Text = "Hero Portrait";
+            this.rbPortrait.UseVisualStyleBackColor = true;
+            this.rbPortrait.CheckedChanged += new System.EventHandler(this.rbPortrait_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 210);
+            this.Controls.Add(this.rbPortrait);
+            this.Controls.Add(this.rbAbility);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbAbility);
+            this.Controls.Add(this.tbHero);
+            this.Controls.Add(this.btnSearch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "OW Icon Finder V2.0";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox tbHero;
+        private System.Windows.Forms.TextBox tbAbility;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.RadioButton rbAbility;
+        private System.Windows.Forms.RadioButton rbPortrait;
+    }
+}
+
---
+++ iconFinder/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Net;
+
+namespace iconFinder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap bitmap;
+        string ability;
+        string hero;
+
+        public string fixStuff(string x, TextBox y)
+        {
+            x = y.Text;
+            x = x.Replace('.'.ToString(), "")
+                 .Replace(':'.ToString(), "")
+                 .Replace('!'.ToString(), "")
+                 .Replace(' '.ToString(), "-");
+            x = x.ToLower();
+            return x;
+        }
+
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (tbHero.Text != "")
+                {
+                    bool done = false;
+                    string link = "";
+                    hero = fixStuff("", tbHero);
+
+
+                    if (rbAbility.Checked)
+                    {
+                        if (tbAbility.Text != "")
+                        {
+                            ability = fixStuff("", tbAbility);
+                            link = String.Format("https://blzgdapipro-a.akamaihd.net/hero/{0}/ability-{1}/icon-ability.png", hero, ability);
+                            done = true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please enter text");
+                        }
+                    }
+                    else
+                    {
+                        link = String.Format("https://d1u1mce87gyfbn.cloudfront.net/hero/{0}/icon-portrait.png", hero);
+                        done = true;
+                    }
+
+                    if (done)
+                    {
+                        btnCopy.Enabled = true;
+                        btnSave.Enabled = true;
+
+                        // Open Webclient and put the image in a bitmap
+                        WebClient webClient = new WebClient();
+                        Stream stream = webClient.OpenRead(link);
+                        bitmap = new Bitmap(stream);
+
+                        // Display the image
+                        pbImage.Image = bitmap;
+                    }
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetImage(bitmap);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "PNG (*.png)|*.png|All files (*.*)|*.*";
+            if (rbAbility.Checked)
+                dialog.FileName = ability;
+            else
+                dialog.FileName = hero;
+            if (dialog.ShowDialog() == DialogResult.OK)
+                bitmap.Save(dialog.FileName);
+        }
+
+        private void rbPortrait_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbPortrait.Checked)
+            {
+                tbAbility.ReadOnly = true;
+                tbAbility.Text = "";
+            }
+            else
+            {
+                tbAbility.ReadOnly = false;
+            }
+                
+        }
+    }
+}
